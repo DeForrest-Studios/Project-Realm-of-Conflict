3a43a931371ea7e42d1bddea9a7b376aaf6dbaed
Robert DeForrest Reynolds <rldr.europa@gmail.com>
2024-01-08 01:02:07 -0800

    Fixed the recruit panel

--- ProjectRoC/Panels/Recruit.py
+++ ProjectRoC/Panels/Recruit.py
@@ -18,38 +18,41 @@ class RecruitPanel(Panel):
                          Interaction=Interaction, ButtonStyle=ButtonStyle)
         Self.SententsPanel = SententsPanel
 
-    async def _Construct_Panel(Self, InfantrySelected=None, InfantryRecruited=None):
+    async def _Construct_Panel(Self, Interaction=None, InfantrySelected=None, InfantryRecruited=False):
         if Self.Interaction.user != Self.InitialContext.author: return
 
-        await Self._Generate_Info(Self.Ether, Self.InitialContext)
+        if Interaction == None:
+            await Self._Generate_Info(Self.Ether, Self.InitialContext)
 
-        Self.RecruitButton = Button(label="Recruit Selection", style=Self.ButtonStyle,
-                                    row=0, custom_id="RecruitButton")
-        Self.RecruitButton.callback = lambda Interaction: Self._Construct_Panel(Self.InfantrySelected, Self.InfantrySelected)
-        Self.BaseViewFrame.add_item(Self.RecruitButton)
+            Self.RecruitButton = Button(label="Recruit Selection", style=Self.ButtonStyle,
+                                        row=0, custom_id="RecruitButton")
+            Self.RecruitButton.callback = lambda Interaction: Self._Construct_Panel(Interaction=Interaction, InfantrySelected=Self.InfantrySelected, InfantryRecruited=True)
+            Self.BaseViewFrame.add_item(Self.RecruitButton)
 
-        Self.InfantyChoices = [SelectOption(label=f"{Infantry} for ${Worth}") for Infantry, Worth in InfantryTable.items()]
-        Self.InfantryChoice = Select(placeholder="Select an Infantry", options=Self.InfantyChoices,
-                                     row=1, custom_id=f"InfantrySelection")
-        Self.InfantryChoice.callback = lambda Interaction: Self._Construct_Panel(Interaction.data["values"][0])
-        Self.BaseViewFrame.add_item(Self.InfantryChoice)
+            Self.InfantyChoices = [SelectOption(label=f"{Infantry} for ${Worth}") for Infantry, Worth in InfantryTable.items()]
+            Self.InfantryChoice = Select(placeholder="Select an Infantry", options=Self.InfantyChoices,
+                                        row=1, custom_id=f"InfantrySelection")
+            Self.InfantryChoice.callback = lambda Interaction: Self._Construct_Panel(Interaction=Interaction, InfantrySelected=Interaction.data["values"][0])
+            Self.BaseViewFrame.add_item(Self.InfantryChoice)
 
-        Self.SententsButton = Button(label="Return to Sentents", style=Self.ButtonStyle,
-                                     row=2, custom_id="SententsButton")
-        Self.SententsButton.callback = lambda Interaction: Self.SententsPanel.__ainit__(Self.Ether, Self.InitialContext, Self.ButtonStyle, Interaction, Self.PlayPanel)
-        Self.BaseViewFrame.add_item(Self.SententsButton)
+            Self.SententsButton = Button(label="Return to Sentents", style=Self.ButtonStyle,
+                                        row=2, custom_id="SententsButton")
+            Self.SententsButton.callback = lambda Interaction: Self.SententsPanel.__ainit__(Self.Ether, Self.InitialContext, Self.ButtonStyle, Interaction, Self.PlayPanel)
+            Self.BaseViewFrame.add_item(Self.SententsButton)
 
-        Self.HomepageButton = Button(label="Home", style=DiscordButtonStyle.grey,
-                                     row=3, custom_id="HomePageButton")
-        # This is a bad callback. This is really bad, I'm well aware. But you know what, fuck it.
-        Self.HomepageButton.callback = lambda Interaction: Self.PlayPanel._Construct_Home(Self.Ether, Self.InitialContext, Interaction)
-        Self.BaseViewFrame.add_item(Self.HomepageButton)
+            Self.HomepageButton = Button(label="Home", style=DiscordButtonStyle.grey,
+                                        row=3, custom_id="HomePageButton")
+            # This is a bad callback. This is really bad, I'm well aware. But you know what, fuck it.
+            Self.HomepageButton.callback = lambda Interaction: Self.PlayPanel._Construct_Home(Self.Ether, Self.InitialContext, Interaction)
+            Self.BaseViewFrame.add_item(Self.HomepageButton)
+        else:
+            Self.Interaction = Interaction
         
         if InfantrySelected:
             Self.InfantrySelected = InfantrySelected
             Self.InfantryChoice.placeholder = InfantrySelected
 
-        if InfantryRecruited:
+        if InfantryRecruited == True:
             InfantryKey = Self.InfantrySelected.split(" for ")[0]
             if Self.Player.Data["Wallet"] >= InfantryTable[InfantryKey]:
                 Self.Player.Data["Wallet"] = round(Self.Player.Data["Wallet"] - InfantryTable[InfantryKey], 2)
