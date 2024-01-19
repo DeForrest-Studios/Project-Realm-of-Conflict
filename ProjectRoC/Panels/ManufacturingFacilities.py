3a629501a82e039ab457666d9299029d4ae790d8
Robert DeForrest Reynolds <rldr.europa@gmail.com>
2024-01-18 23:30:56 -0800

    You can now rename manufacturing facilities

--- ProjectRoC/Panels/ManufacturingFacilities.py
+++ ProjectRoC/Panels/ManufacturingFacilities.py
@@ -3,7 +3,7 @@ from discord.ext.commands import Context as DiscordContext
 from discord import Interaction as DiscordInteraction
 from discord import ButtonStyle as DiscordButtonStyle
 from discord import SelectOption, Embed
-from discord.ui import Button, Select, View
+from discord.ui import Button, Select, View, Modal, TextInput
 from Panels.Panel import Panel
 from Structures import ManufacturingFacility
 
@@ -16,6 +16,14 @@ class ManufacturingFacilitiesPanel(Panel):
     async def _Construct_Panel(Self, Interaction=None, FacilitySelected=None,
                                GroupSelected=None, BoughtFacility=False,
                                BoughtLandPlot=False):
+        if type(Interaction) == tuple:
+            if len(Interaction) == 2:
+                Data = Interaction
+                Interaction = Data[0]
+                FacilitySelected = Data[1]
+            elif len(Interaction) == 3:
+                GroupSelected = Data[2]
+
         if Self.Interaction.user != Self.InitialContext.author: return
 
         if Interaction is not None:
@@ -59,13 +67,13 @@ class ManufacturingFacilitiesPanel(Panel):
         if FacilitySelected is not None:
             Self.FacilitySelected = FacilitySelected
             Self.FacilitySelection.placeholder = FacilitySelected
-            Self.EmbedFrame.description += f"{Self.Player.ManufacturingFacilities[Self.FacilitySelected].Data['Name']}\n"
+            Self.EmbedFrame.description += f"**{Self.Player.ManufacturingFacilities[Self.FacilitySelected].Data['Name']}**\n"
             Self.EmbedFrame.description += f"**Level:** {Self.Player.ManufacturingFacilities[Self.FacilitySelected].Data['Level']}\n"
             Self.EmbedFrame.description += f"**Recipe:** {Self.Player.ManufacturingFacilities[Self.FacilitySelected].Data['Recipe']}\n"
 
-            Self.ChangeFacilityNameButton = Button(label="Change Facility Name (WIP)", style=Self.ButtonStyle, row=1, custom_id="ChangeFacilityNameButton")
+            Self.ChangeFacilityNameButton = Button(label="Change Facility Name", style=Self.ButtonStyle, row=1, custom_id="ChangeFacilityNameButton")
             if hasattr(Self, "GroupSelected"):
-                Self.ChangeFacilityNameButton.callback = lambda Interaction: Self._Send_Change_Name_Modal((Interaction, Self.GroupSelected, Self.FacilitySelected))
+                Self.ChangeFacilityNameButton.callback = lambda Interaction: Self._Send_Change_Name_Modal((Interaction, Self.FacilitySelected, Self.GroupSelected))
             else:
                 Self.ChangeFacilityNameButton.callback = lambda Interaction: Self._Send_Change_Name_Modal((Interaction, Self.FacilitySelected))
             Self.BaseViewFrame.add_item(Self.ChangeFacilityNameButton)
@@ -99,4 +107,27 @@ class ManufacturingFacilitiesPanel(Panel):
 
 
     async def _Send_Change_Name_Modal(Self, Data):
-        await Self._Construct_Panel(*Data)
+        Interaction = Data[0]
+        if Interaction.user != Self.InitialContext.author:
+            return
+        Self.ChangeFacilityNameModal = Modal(title="Change Facility Name")
+        Self.ChangeFacilityNameModal.on_submit = lambda ButtonInteraction: Self._Change_Facility_Name((ButtonInteraction,) + Data[1::])
+
+        Self.FacilityName = TextInput(label="Enter new facility name:")
+        Self.ChangeFacilityNameModal.add_item(Self.FacilityName)
+        await Interaction.response.send_modal(Self.ChangeFacilityNameModal)
+
+    
+    async def _Change_Facility_Name(Self, Data):
+        Interaction, FacilitySelected = Data[0], Data[1]
+        if len(Data) == 3: GroupSelected = Data[2]
+        Self.Player.ManufacturingFacilities[Self.FacilityName.value] = Self.Player.ManufacturingFacilities[FacilitySelected]
+        Self.Player.ManufacturingFacilities.pop(FacilitySelected)
+        FacilitySelected = Self.FacilityName.value
+        Self.Player.ManufacturingFacilities[FacilitySelected].Data["Name"] = FacilitySelected
+
+        if len(Data) == 2:
+            await Self._Construct_Panel(Interaction=Interaction, FacilitySelected=FacilitySelected)
+        else:
+            await Self._Construct_Panel(Interaction=Interaction, FacilitySelected=FacilitySelected, GroupSelected=GroupSelected)
+
