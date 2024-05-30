b89a607a244ff43236adb09e44c28c9bac5501bf
robert-deforrest-reynolds <rldr.europa@gmail.com>
2024-05-30 08:49:29 -0700

    Small cleanup

--- ProjectRoC/Commence.py
+++ ProjectRoC/Commence.py
@@ -9,23 +9,32 @@ from sys import exit
 from sys import argv
 
 if __name__ == '__main__':
-    KeyUser = argv[1]
+    KeyUser = argv[1] # Pass in the key we'd like to launch with
     Ether = RealmOfConflict()
     
     @Ether.command(aliases=["oc", "oC", "OC"])
     async def Play_Interaction(InitialContext: Context):
-        if await Ether.Guild_Guard(InitialContext) == "Unprotected":
-            return
-        await InitialContext.message.delete()
-        if InitialContext.author.id not in Ether.Data["Players"].keys() or Ether.Data["Players"][InitialContext.author.id].Data["Team"] is None:
+        # The "Initial Contex" is the original message that the user sends to receive the panel
+        if await Ether.Guild_Guard(InitialContext) == "Unprotected": return
+        
+        # If the user's ID has not been added to the database as a player, or for some reason if they do not
+        # have a team, send them a warm welcome
+        if InitialContext.author.id not in Ether.Data["Players"].keys() or\
+        Ether.Data["Players"][InitialContext.author.id].Data["Team"] is None:
             await Ether.Send_Welcome(InitialContext.author)
-        elif InitialContext.author.id in Ether.Data["Panels"].keys():
+            return
+
+        # If the player already has a panel, delete the previous panel before sending them a new one
+        if InitialContext.author.id in Ether.Data["Panels"].keys():
             await Ether.Data["Panels"][InitialContext.author.id].DashboardMessage.delete()
             Ether.Data["Panels"].update({InitialContext.author.id:PlayPanel(Ether, InitialContext)})
-        else:
-            Ether.Data["Panels"].update({InitialContext.author.id:PlayPanel(Ether, InitialContext)})
+            return
+            
+        # Default send message behavior
+        await InitialContext.message.delete()
+        Ether.Data["Panels"].update({InitialContext.author.id:PlayPanel(Ether, InitialContext)})
 
-    # Override of existing on_ready from discord.py
+    # Override of existing on_guild_available from discord.py
     @Ether.event
     async def on_guild_available(Guild):
         Ether.Logger.info(f"Guild available: {Guild.name}")
@@ -36,8 +45,8 @@ if __name__ == '__main__':
         Ether.Guild = Ether.guilds[0]
 
         # Official Server Setup
-        if Ether.Guild.id == 1190386754327412867:
-            Ether.Logger.info("Running on Developer Server")
+        if Ether.Guild.id == 1190386754327412867: # Leave this all hard coded for very good reason.
+            Ether.Logger.info("Running on Official Server")
             Ether.Roles = {
                 "Titan":Ether.Guild.get_role(1190386754327412870),
                 "Analis":Ether.Guild.get_role(1190386754327412869),
@@ -47,7 +56,7 @@ if __name__ == '__main__':
             Ether.Data.update({"Simulation Channel": Ether.Guild.get_channel(1190386761831039096)})
 
         # Dev Server Setup
-        if Ether.Guild.id == 1190385562604015626:
+        if Ether.Guild.id == 1190385562604015626: # Leave this all hard coded for very good reason.
             Ether.Dev_Mode()
             Ether.Logger.info("Running on Developer Server")
             Ether.Roles = {
